e6fa6f6899113bca73b2e782037c11f9ed3b158d
Austin Keeton <austinwkeeton@protonmail.com>
2021-12-10 18:59:47 -0600

    Solve day 2 part 1.

--- 02.rkt
+++ 02.rkt
@@ -16,13 +16,21 @@
         (define units (string->number (cadr command)))
         (list instruction units)))))
 
-(println (take (get-input) 5))
 ;;------------------------------------------------------------------------------
 ;; Part 1
 ;;------------------------------------------------------------------------------
 
-(define (solve-part-1)
-  (error "unimplemented"))
+(define (solve-part-1) ;=> 1714680
+  (for/fold ([horiz 0]
+             [depth 0]
+             #:result (* horiz depth))
+             ([command (in-list (get-input))])
+    (define direction (first command))
+    (define distance (second command))
+    (match direction
+      ["up" (values horiz (- depth distance))]
+      ["down" (values horiz (+ depth distance))]
+      ["forward" (values (+ horiz distance) depth)])))
 
 ;;------------------------------------------------------------------------------
 ;; Part 2
