594d6c7ab277567f7101dfc11102a2d928abc8f2
Austin Keeton <austinwkeeton@protonmail.com>
2021-12-12 07:24:56 -0600

    WIP.

--- 03.rkt
+++ 03.rkt
@@ -6,13 +6,16 @@
 
 (define input-path "03.txt")
 
-;=> '(#(#\0 #\0 #\1) ...)
-(define (get-input)
+(define (get-input) ;=> '((1 0 1 0 1) (0 1 0 1 0) ...)
   (call-with-input-file
     input-path
     (λ (in)
       (for/list ([line (in-lines in)])
-        (list->vector (string->list line))))))
+        (bitstr->bitlist line)))))
+
+(define (bitstr->bitlist str) ;=> '(1 0 1 0 1)
+  (for/list ([c (in-string str)])
+    (if (eq? c #\0) 0 1)))
 
 ;;------------------------------------------------------------------------------
 ;; Part 1
@@ -25,7 +28,20 @@
             ([bits (in-list (get-input))])
     (values (add1 rows) (tally-bits tally bits))))
 
-(define (tally-bits tally bits) #())
+(define (tally-bits tally bits)
+  (for/list ([t (in-list tally)]
+             [b (in-list bits)])
+    (+ t b)))
+
+(define (gamma-rate bits rows)
+  (for/fold ([gbits '()]
+             #:result (string->number (list->string gbits) 2))
+            ([b (in-list bits)])
+    (if (> b (/ rows 2)) 1 0)))
+
+(define (epsilon-rate bits rows)
+  (for/list ([b (in-list bits)])
+    (if (> b (/ rows 2)) 0 1)))
 
 (define (power-consumption tally rows) 0)
 
