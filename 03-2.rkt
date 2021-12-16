688a3a4f28dad276cb57403fc6457c19afee88c7
Austin Keeton <austinwkeeton@protonmail.com>
2021-12-15 20:16:29 -0600

    Solve day 3 part 2.

--- 03-2.rkt
+++ 03-2.rkt
@@ -7,30 +7,57 @@
 (define input-path "03.txt")
 (define sample-input-path "03-sample.txt")
 
-(define (parse-input file-path) ;=> list<vec<num>>
+
+
+(define (solve)
+  (life-rating (file->data input-path)))
+
+
+
+(define (file->data path)
   (call-with-input-file
-    file-path
-    (lambda (file-port)
+    path
+    (λ (file-port)
       (for/list ([line (in-lines file-port)])
         (list->vector (map (λ (c)
                              (if (eq? c #\0) 0 1))
                            (string->list line)))))))
 
-(define (oxygen-rating data)
+
+
+(define (life-rating data)
+  (* (oxy-rating data) (co2-rating data)))
+
+(define (oxy-rating data)
+  (rating data oxy-comparator))
+
+(define (co2-rating data)
+  (rating data co2-comparator))
+
+(define (oxy-comparator zeros ones)
+  (cond [(> (length zeros) (length ones)) zeros]
+        [(< (length zeros) (length ones)) ones]
+        [else ones]))
+
+(define (co2-comparator zeros ones)
+  (cond [(> (length zeros) (length ones)) ones]
+        [(< (length zeros) (length ones)) zeros]
+        [else zeros]))
+
+
+
+(define (rating data comparator)
   (define num-cols (vector-length (car data)))
   (for/fold ([remaining data]
              #:result (bitvec->num (car remaining)))
             ([col-idx (in-range num-cols)])
              #:break (= 1 (length remaining))
-    (filter-on-col remaining col-idx)))
+    (filter-on-col remaining col-idx comparator)))
 
-(define (filter-on-col data col)
+(define (filter-on-col data col comparator)
   (for/fold ([zeros '()]
              [ones '()]
-             #:result (cond
-                        [(> (length zeros) (length ones)) zeros]
-                        [(< (length zeros) (length ones)) ones]
-                        [else ones]))
+             #:result (comparator zeros ones))
             ([row (in-list data)])
     (if (= 0 (vector-ref row col))
         (values (cons row zeros) ones)
@@ -42,7 +69,7 @@
             ([bit (in-vector bitvec)])
     (string-append str (number->string bit))))
 
-(define (solve-part-2) 0)
+
 
 ;;------------------------------------------------------------------------------
 ;; Tests
@@ -65,24 +92,19 @@
         #(0 1 0 1 0)))
 
     (test-case "parse-input"
-      (check-equal? (parse-input sample-input-path)
-                    normalized-input))
+      (check-equal? (file->data sample-input-path) normalized-input))
 
     (test-case "bitvec->num"
-      (check-eq? (bitvec->num #(0 1 1 0 1 1 0 1 0 1))
-                 437))
+      (check-eq? (bitvec->num #(0 1 1 0 1 1 0 1 0 1)) 437))
 
-    (test-case "filter-on-col"
-      (check-equal? (filter-on-col normalized-input 0)
-                    ; The procedure reverses the list during filtering
-                    '(#(1 1 0 0 1)
-                      #(1 0 0 0 0)
-                      #(1 1 1 0 0)
-                      #(1 0 1 0 1)
-                      #(1 0 1 1 1)
-                      #(1 0 1 1 0)
-                      #(1 1 1 1 0))))
+    (test-case "oxy-rating"
+      (check-eq? (oxy-rating normalized-input) 23))
 
-    (test-case "oxygen-rating"
-      (check-eq? (oxygen-rating normalized-input) 23))
-)
+    (test-case "co2-rating"
+      (check-eq? (co2-rating normalized-input) 10))
+
+    (test-case "life-rating"
+      (check-eq? (life-rating normalized-input) 230))
+
+    (test-case "solve"
+      (check-eq? (solve) 7041258)))
